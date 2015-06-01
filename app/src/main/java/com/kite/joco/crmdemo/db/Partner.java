c689c0e88ae9c4002b897aa05dd4a9c4f2824445
mesterj <mesterj@freemail.hu>
2015-06-01 16:08:34 +0200

    Initial commit

---
+++ app/src/main/java/com/kite/joco/crmdemo/db/Partner.java
@@ -0,0 +1,15 @@
+package com.kite.joco.crmdemo.db;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by Mester József on 2015.06.01..
+ */
+public class Partner extends SugarRecord<Partner> {
+
+    String nev;
+    String irsz;
+    String falu;
+    String utca;
+
+}
